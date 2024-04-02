719f92caf84dd4a0956c3964803eae698064274b
Shams <skm478@gmail.com>
2024-04-02 14:55:50 -0400

    Get inventory route

--- Server/app.py
+++ Server/app.py
@@ -8,6 +8,7 @@ from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request, session
 import os
+from sqlalchemy.exc import SQLAlchemyError
 
 # Local imports
 
@@ -102,6 +103,51 @@ def cards():
 
 ######3Inventory Queries######################
 
+@app.route('/inventory<int:id>', methods = ['GET', 'DELETE'])
+def Userinventory(id):
+
+    filter_mapping = {
+        'name': lambda value: Card.name.contains(value) , #SQLalchemy binary expression type is the returnfrom lambda function
+        'card_code' : lambda value: CardinSet.card_code.contains(value),
+        'rarity' : lambda value: CardinSet.rarity.ilike(f'%{value}%'),
+        'card_type' : lambda value: Card.card_type.ilike(f'%{value}%'), 
+    }
+
+    skip_keys = ['page', 'per_page']
+    
+    if request.method == 'GET':
+        try:                
+            inventory_filtered_query = Inventory.query.filter(Inventory.user_id == id) #Base Query we need to add filter parameters
+            
+            for key, value in request.args.items():
+                if key in filter_mapping:
+                    filter_element = filter_mapping[key](value)
+                    inventory_filtered_query = inventory_filtered_query.filter(filter_element)
+
+            page = request.args.get('page', default=1,type=int)
+            per_page = request.args.get('per_page', default=20,type=int)
+            paginated_inventory = paginate(inventory_filtered_query,page,per_page)
+
+            card_list = [card.to_dict(rules=('-cardinSet.card.card_in_deck',)) for card in paginated_inventory.items]
+
+            response_data = {
+                'cards': card_list,
+                'page': page,
+                'per_page' : per_page,
+                'total_pages' : paginated_inventory.pages,
+                'total_items' : paginated_inventory.total
+            }
+            response = make_response(jsonify(response_data),200)
+        except SQLAlchemyError as se:
+            error_message = f'Error {se}'
+            return make_response(jsonify({'error': error_message}), 500)
+        except Exception as e:
+            error_message = f'Error {e}'
+            return make_response(jsonify({'error': error_message}), 500)
+    else:
+        pass #DELETE inventory 
+
+    return response
 
 ######Reconciliation Routes################
 
