a92a64d92d1a66618a11c748ca75f3a124701bd5
Jeremy Cook <jcook61592@gmail.com>
2024-04-30 16:34:54 -0400

    Added react hook form

--- Client/screens/CreateUser.js
+++ Client/screens/CreateUser.js
@@ -1,22 +1,21 @@
 import { Button, Text, TextInput, StyleSheet, View } from "react-native";
-import {useState} from 'react';
+import { useState } from "react";
 import { BASE_URL } from "../index";
 
-export default function CreateUser() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+export default function CreateUser({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-  }
-
+  const handleSubmit = () => {
+    navigation.navigate("Login");
+  };
 
   return (
     <View>
       <TextInput placeholder="Enter Username" />
       <TextInput placeholder="Enter Password" />
       <TextInput placeholder="Confirm Password" />
-      <Button title="Create User" onPress={() => handleSubmit} />
+      <Button title="Create User" onPress={handleSubmit} />
     </View>
   );
 }
