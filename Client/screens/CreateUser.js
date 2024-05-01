d1991c06a5e7bef1f37d96b318c2a75de7de776b
Jeremy Cook <jcook61592@gmail.com>
2024-05-01 16:51:26 -0400

    Updated CreateUser screen with form structure

--- Client/screens/CreateUser.js
+++ Client/screens/CreateUser.js
@@ -1,31 +1,80 @@
-import { Button, Text, TextInput, StyleSheet, View } from "react-native";
-import { useState } from "react";
-import { BASE_URL } from "../index";
+import { Text, View, TextInput, Button, Alert, StyleSheet } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
-export default function CreateUser({ navigation }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = () => {
-    navigation.navigate("Login");
-  };
+export default function CreateUser() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
 
   return (
     <View>
-      <TextInput placeholder="Enter Username" />
-      <TextInput placeholder="Enter Password" />
-      <TextInput placeholder="Confirm Password" />
-      <Button title="Create User" onPress={handleSubmit} />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Username"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="username"
+      />
+      {errors.username && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Confirm Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="confirmPassword"
+      />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     textAlign: "center",
+//   },
+// });
