f061ee49d4c64478f4e6c29e589dd87de727ea8b
Jeremy Cook <Jcook61592@gmail.com>
2024-07-17 19:03:40 -0400

    first commit: added search to utility

--- Client/utility.js
+++ Client/utility.js
@@ -1,3 +1,15 @@
-const storeAcessToken = async (accessToken) => {
+import BASE_URL from './config';
+
+const storeAccessToken = async (accessToken) => {
   await SecureStore.setItemAsync("accessToken", accessToken);
 };
+
+export const search = async (params) => {
+  try {
+    const response = await fetch(`${BASE_URL}/card/${params}`)
+    const data = response.json()
+  }
+  catch (error) {
+    console.error('Could not retrieve card info:', error)
+  }
+}
