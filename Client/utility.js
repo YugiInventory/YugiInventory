030f04e64686a258555e4403faa91c4e6b15bed3
Jeremy Cook <jcook61592@gmail.com>
2024-09-13 16:09:45 -0400

    updated import to reflect new location

--- Client/utility.js
+++ Client/utility.js
@@ -1,4 +1,4 @@
-import BASE_URL from "./config";
+import BASE_URL from "./services/AuthFunctions";
 
 const storeAccessToken = async (accessToken) => {
   await SecureStore.setItemAsync("accessToken", accessToken);
