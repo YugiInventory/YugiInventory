8ddf9370943c80b62d58fadec1d834473ac7c158
Jeremy Cook <Jcook61592@gmail.com>
2024-10-10 16:18:07 -0400

    added press functions for quantity modal

--- Client/components/QuantityModal.js
+++ Client/components/QuantityModal.js
@@ -7,11 +7,18 @@ import {
 import * as SecureStore from "expo-secure-store";
 
 const QuantityModal = ({ item }) => {
+  const increase = (item) => {
+    console.log("increase", item.quantity);
+  };
+  const decrease = (item) => {
+    console.log("decrease", item.quantity);
+  };
+
   return (
     <View>
-      <Button title="+" />
+      <Button title="+" onPress={increase} item={item} />
       <Text style={styles.quantityCount}>{item.quantity}</Text>
-      <Button title="-" />
+      <Button title="-" onPress={decrease} item={item} />
     </View>
   );
 };
